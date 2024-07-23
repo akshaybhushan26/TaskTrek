47304c3b8457c8d766b496d0141fdf64fe20ea35
Akshay Bhushan <121752750+akshaybhushan26@users.noreply.github.com>
2024-07-23 11:01:21 +0530

    first commit

---
+++ lib/data/database.dart
@@ -0,0 +1,24 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class ToDoDataBase{
+
+  List toDoList = [];
+  //reference our box
+  final _mybox = Hive.box('mybox');
+
+  //run this method id this is the 1st time ever opening this app.
+  void createInitialData(){
+    toDoList = [
+      ["Do Homework",false],
+      ["Do Exercise",false]
+    ];
+  }
+  //load the data from database
+  void loadData(){
+    toDoList = _mybox.get("TODOLIST");
+  }
+  //update the database
+  void updateDataBase(){
+    _mybox.put("TODOLIST", toDoList);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'pages/home_page.dart';
+
+void main() async{
+
+  //initialising hive
+  await Hive.initFlutter();
+  // open a box 
+  var box =  await Hive.openBox('mybox');
+
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.blue), //helps us describing default color
+    );
+  }
+}
